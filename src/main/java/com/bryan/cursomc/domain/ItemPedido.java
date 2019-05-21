2c157e97fd8fe5ecc38da39c7b693e2de2c64afe
Michael Bryan <michaelbryan.eng@gmail.com>
2019-05-21 16:14:27 -0300

    Subtotal e total do pedido

--- src/main/java/com/bryan/cursomc/domain/ItemPedido.java
+++ src/main/java/com/bryan/cursomc/domain/ItemPedido.java
@@ -37,6 +37,9 @@ public class ItemPedido implements Serializable {
 		return id.getPedido();
 	}
 
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
 	
 	public Produto getProduto() {
 		return id.getProduto();
