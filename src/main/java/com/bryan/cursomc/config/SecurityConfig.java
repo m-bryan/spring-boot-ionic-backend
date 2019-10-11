697f96e7317bfea403b2fd61d2a58cc37dba6219
Michael Bryan <michaelbryan.eng@gmail.com>
2019-10-11 14:50:52 -0300

    Enviando imagem via endpoint

--- src/main/java/com/bryan/cursomc/config/SecurityConfig.java
+++ src/main/java/com/bryan/cursomc/config/SecurityConfig.java
@@ -43,12 +43,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes/**"
+			"/clientes",
+			"/clientes/picture",
+			"/auth/forgot/**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS = {
-			"/h2-console/**",
-			"/auth/forgot/**"
+			"/h2-console/**"
 	};
 
 	@Override
@@ -60,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
